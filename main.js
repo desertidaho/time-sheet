9625691a7af062c82f12397e9ebf7507a88df282
Brett Wilcox <wilcox.brett@gmail.com>
2019-02-07 15:53:58 -0700

    Working app.

--- main.js
+++ main.js
@@ -1,5 +1,68 @@
-let week1 = [0, 0, 0, 0, 0];
-let week1Total = 0;
+let week = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
 
-let week2 = [0, 0, 0, 0, 0];
-let week2Total = 0;
+function addHour(i, j) {
+  week[i][j]++
+
+  calculateWeek1Total()
+  calculateWeek2Total()
+}
+
+function subHour(i, j) {
+  if (week[i][j] != 0) {
+    week[i][j]--
+  }
+
+
+  calculateWeek1Total()
+  calculateWeek2Total()
+}
+
+
+let week1Total = 0
+let week2Total = 0
+
+function calculateWeek1Total() {
+  let add = (a, b) => a + b
+  week1Total = week[0].reduce(add);
+
+  calculateCummulativeTotal()
+}
+
+function calculateWeek2Total() {
+  let add = (a, b) => a + b
+  week2Total = week[1].reduce(add);
+
+  calculateCummulativeTotal()
+}
+
+let grand = 0;
+let pay = 0;
+
+function calculateCummulativeTotal() {
+  grand = week1Total + week2Total;
+  pay = grand * 75
+
+  draw()
+}
+
+function draw() {
+  document.getElementById('w1m').innerText = week[0][0].toString();
+  document.getElementById('w1tu').innerText = week[0][1].toString();
+  document.getElementById('w1w').innerText = week[0][2].toString();
+  document.getElementById('w1th').innerText = week[0][3].toString();
+  document.getElementById('w1f').innerText = week[0][4].toString();
+
+  document.getElementById('w2m').innerText = week[1][0].toString();
+  document.getElementById('w2tu').innerText = week[1][1].toString();
+  document.getElementById('w2w').innerText = week[1][2].toString();
+  document.getElementById('w2th').innerText = week[1][3].toString();
+  document.getElementById('w2f').innerText = week[1][4].toString();
+
+  document.getElementById('week1Total').innerText = week1Total.toString();
+  document.getElementById('week2Total').innerText = week2Total.toString();
+  document.getElementById('grandTotal').innerText = grand.toString();
+  document.getElementById('grandPay').innerText = `$ ${pay.toString()}`;
+}
